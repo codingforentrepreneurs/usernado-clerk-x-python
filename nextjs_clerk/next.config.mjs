a25c98476c7a50e46f508699dd6414f67766c93b
Justin Mitchel <jmitchel3@users.noreply.github.com>
2025-05-02 11:33:13 -0600

    27 - Configure Redirects

--- nextjs_clerk/next.config.mjs
+++ nextjs_clerk/next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+    return [
+        {
+        source: '/signin',
+        destination: '/login',
+        permanent: true,
+        },
+        {
+        source: '/sign-in',
+        destination: '/login',
+        permanent: true,
+        },
+        {
+        source: '/sign-up',
+        destination: '/register',
+        permanent: true,
+        },
+        {
+        source: '/signup',
+        destination: '/register',
+        permanent: true,
+        },
+    ]
+    },
+};
 
 export default nextConfig;
